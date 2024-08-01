ac4d10c82e9f2c85cf16a278ae49aee282ac2cbd
kevinrossetti <105368440+kevinrossetti@users.noreply.github.com>
2024-08-01 09:21:09 -0300

    save

--- main.js
+++
@@ -1,4 +0,0 @@
-document.querySelector('form').addEventListener('submit', function(event) {
-    event.preventDefault(); // Previne o envio real do formulário
-    alert('Formulário enviado com sucesso!');
-});
